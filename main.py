270c852fa440d696f9eb2a462d59fda4868c841a
Shuflduf <96599330+Shuflduf@users.noreply.github.com>
2024-07-09 03:41:37 +0000

    when a user sends a message it should get logged to a specific channel

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+token = os.environ['BOT_TOKEN']
+target = os.environ['TARGET_ID']
+log_channel = os.environ['BOT_TOKEN']
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    client.status = discord.Status.invisible
+    await client.change_presence(status=discord.Status.invisible)
+
+
+@client.event
+async def on_message(message):
+    if message.author.id == target:
+        channel = client.get_channel(int(log_channel))
+        await channel.send(message.content)
+
+
+client.run(token)
